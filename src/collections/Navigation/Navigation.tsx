c6a0c5fef749042ee9cab5e0ac07ad540821e68e
Joe <95106688+Manitec@users.noreply.github.com>
2023-04-19 21:55:12 -0400

    Update Navigation.tsx

--- src/collections/Navigation/Navigation.tsx
+++ src/collections/Navigation/Navigation.tsx
@@ -6,11 +6,8 @@ import { togglePopup } from "services";
 interface NavigationProps {
   ref?: RefObject<HTMLButtonElement>;
 }
-
 export const Navigation: React.FC<NavigationProps> = ({ ...props }) => {
   const dispatch = useAppDispatch();
-  return (
-    <nav className="navigation-landing" {...props}>
       <Logo />
       <Popup
         closePopup={() => togglePopup("null")}
