256c5e001d90a7d480968d49293ae455fbebe560
Joe <95106688+Manitec@users.noreply.github.com>
2023-04-19 21:59:31 -0400

    Update Navigation.tsx

--- src/collections/Navigation/Navigation.tsx
+++ src/collections/Navigation/Navigation.tsx
@@ -1,13 +1,20 @@
 import { Button, Logo, Popup } from "components";
 import { useAppDispatch, useAppSelector } from "hooks";
 import React, { RefObject } from "react";
-import { togglePopup } from "services";
-
 interface NavigationProps {
   ref?: RefObject<HTMLButtonElement>;
 }
+
 export const Navigation: React.FC<NavigationProps> = ({ ...props }) => {
   const dispatch = useAppDispatch();
-     </nav>
+  const { popupOpened } = useAppSelector((state) => state.counter);
+  return (
+    <nav className="navigation-landing" {...props}>
+      <Logo />
+      <Popup
+        closePopup={() => togglePopup("null")}
+        popupType={popupOpened || "null"}
+      />
+    </nav>
   );
 };
