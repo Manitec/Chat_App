23d50a74bb0ada5da5ba29ec3c2499d01ab7ad45
Joe <95106688+Manitec@users.noreply.github.com>
2023-04-19 22:02:58 -0400

    Update Navigation.tsx

--- src/collections/Navigation/Navigation.tsx
+++ src/collections/Navigation/Navigation.tsx
@@ -1,7 +1,6 @@
 import { Button, Logo, Popup } from "components";
 import { useAppDispatch, useAppSelector } from "hooks";
 import React, { RefObject } from "react";
-import { togglePopup } from "services";
 
 interface NavigationProps {
   ref?: RefObject<HTMLButtonElement>;
