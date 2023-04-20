ed9830b33580e0407562feb7cadb239d1b5bfd66
Joe <95106688+Manitec@users.noreply.github.com>
2023-04-19 21:32:25 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,12 @@ import dynamic from "next/dynamic";
 const LandingPageHero = dynamic(() =>
   import("sections").then((el) => el.LandingPageHero)
 );
-const Section1 = dynamic(() => import("sections").then((el) => el.Section1), {
-  loading: () => <Loader />,
-});
-const Section2 = dynamic(() => import("sections").then((el) => el.Section2), {
-  loading: () => <Loader />,
-});
-const Section3 = dynamic(() => import("sections").then((el) => el.Section3), {
-  loading: () => <Loader />,
-});
-const Section4 = dynamic(() => import("sections").then((el) => el.Section4), {
-  loading: () => <Loader />,
-});
 
 export default function Home() {
   return (
     <>
       <LandingPageHero />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
+  
     </>
   );
 }
